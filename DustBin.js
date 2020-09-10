384432707f6c9976ea976b83165127b2ce6f8901
navya-code8 <70181906+navya-code8@users.noreply.github.com>
2020-09-09 18:39:18 -0700

    Add files via upload

---
+++ DustBin.js
@@ -0,0 +1,22 @@
+
+class DustBin{
+
+    constructor(x,y,width,height){
+        this.color = "whiteJ";
+        this.body = Bodies.rectangle(x,y,width,height, {restitution:1,isStatic:true});
+        this.width = width;
+        this.height = height;
+       
+        World.add(world, this.body);
+    }
+
+    display(){
+
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill(this.color)
+        rect(pos.x, pos.y, this.width, this.height);
+        
+
+    }
+}
